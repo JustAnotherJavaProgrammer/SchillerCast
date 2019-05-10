3d45b281e0693d4de96e664a59ae5faca4827ce1
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2019-05-11 00:44:25 +0200

    Kleinere Änderungen, um Netzwerkfunktionen einzuführen

--- src/headless/Headless.java
+++ src/headless/Headless.java
@@ -17,6 +17,10 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Headless extends JFrame {
 
@@ -48,6 +52,19 @@ public class Headless extends JFrame {
 	 */
 	public Headless() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				System.exit(0);
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {}
+		});
 		try {
 			UIManager.setLookAndFeel(new MaterialLookAndFeel());
 		} catch (UnsupportedLookAndFeelException e) {
@@ -63,7 +80,14 @@ public class Headless extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		JLabel lblWaitingForConnections = new JLabel("Waiting for connections...");
+		JLabel lblWaitingForConnections;
+		try {
+			lblWaitingForConnections = new JLabel(
+					"Waiting for connections on '" + InetAddress.getLocalHost().getCanonicalHostName() + "'...");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			lblWaitingForConnections = new JLabel("Waiting for connections...");
+		}
 		lblWaitingForConnections.setFont(lblWaitingForConnections.getFont().deriveFont(getWidth() / 25f));
 		lblWaitingForConnections.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblWaitingForConnections, BorderLayout.SOUTH);
@@ -72,7 +96,7 @@ public class Headless extends JFrame {
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setFont(lblTitle.getFont().deriveFont(Font.BOLD, getWidth() / 10f));
 		contentPane.add(lblTitle, BorderLayout.CENTER);
-		
+
 		JLabel lblNewLabel = new JLabel("Development version - Not yet suitable for use in production");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.TRAILING);
 		contentPane.add(lblNewLabel, BorderLayout.NORTH);
